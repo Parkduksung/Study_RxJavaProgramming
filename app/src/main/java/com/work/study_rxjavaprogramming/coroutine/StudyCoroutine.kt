00c1f3f20dca9b8a8514faebd1f4a457ca4d52cd
Parkduksung <duksung1234@naver.com>
2021-01-14 23:47:08 +0900

    viewmodel에서의 코루틴 적용 - 기본적인 뷰모델 생성 및 dataBinding 적용.

--- app/src/main/java/com/work/study_rxjavaprogramming/coroutine/StudyCoroutine.kt
+++ app/src/main/java/com/work/study_rxjavaprogramming/coroutine/StudyCoroutine.kt
@@ -1,90 +1,28 @@
 package com.work.study_rxjavaprogramming.coroutine
 
 import android.os.Bundle
-import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.work.study_rxjavaprogramming.BaseActivity
 import com.work.study_rxjavaprogramming.R
 import com.work.study_rxjavaprogramming.databinding.ActivityCoroutineBinding
-import com.work.study_rxjavaprogramming.ext.textChangeToFlow
-import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import kotlin.coroutines.CoroutineContext
 
 class StudyCoroutine : BaseActivity<ActivityCoroutineBinding>(R.layout.activity_coroutine) {
 
-    private val myCoroutineJob = Job()
-
-    private val jobOne = GlobalScope.launch {
-        for(i in 0..10){
-            Log.d("결과", i.toString())
-            delay(1000)
-        }
+    private val studyCoroutineViewModel: StudyCoroutineViewModel by lazy {
+        ViewModelProvider(this).get(
+            StudyCoroutineViewModel::class.java
+        )
     }
-
-    private val jobTwo = GlobalScope.async {
-            var sum = 0
-            for (i in 0..10) {
-                sum += i
-                delay(500)
-            }
-            Log.d("결과", sum.toString())
-            sum
-    }
-
-    //순서 = c - a - b - d
-    private fun jobThree() {
-        runBlocking { // this: CoroutineScope
-            launch {
-                delay(200L)
-                println("a")
-            }
-
-            coroutineScope { // Creates a coroutine scope
-                launch {
-                    delay(500L)
-                    println("b")
-                }
-
-                delay(100L)
-                println("c") // This line will be printed before the nested launch
-            }
-
-            println("d") // This line is not printed until the nested launch completes
-        }
-    }
-
-    private val myCoroutineContext: CoroutineContext
-        get() = Dispatchers.IO + myCoroutineJob + jobOne + jobTwo
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
-        GlobalScope.launch(myCoroutineContext) {
-
-            jobThree()
-
-            val editTextFlow =
-                binding.etText.textChangeToFlow()
-
-            editTextFlow
-                .debounce(1500)
-                .filter {
-                    it?.length!! > 0
-                }
-                .onEach {
-                    Log.d("결과", it.toString())
-                }
-                .launchIn(this)
+        binding.run {
+            viewModel = studyCoroutineViewModel
+            lifecycleOwner = this@StudyCoroutine
         }
 
+
     }
 
-    override fun onDestroy() {
-        myCoroutineContext.cancel()
-        super.onDestroy()
-    }
 }
---
+++ app/src/main/java/com/work/study_rxjavaprogramming/coroutine/StudyCoroutineViewModel.kt
@@ -0,0 +1,12 @@
+package com.work.study_rxjavaprogramming.coroutine
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class StudyCoroutineViewModel : ViewModel() {
+
+
+    val editText = MutableLiveData<String>()
+
+
+}
